8babca799074719755aa09d202cede5c8d63c0fc
Yonas Sh <yonakifle@yahoo.com>
2021-09-14 19:39:23 -0700

    Calculator

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num1=4;
+        int num2 =6;
+        System.out.println(num1 + num2);
+	}
+
+}
